7fde5b8fe44e7f24bdd0cbcf9d25b4a5a2cab80a
killuaxhunterx <arthurnegaodebsb@gmail.com>
2025-11-14 23:32:39 -0300

    Forma de acesasr o proximo estado adicionado

--- src/messageHandler/Name.py
+++ src/messageHandler/Name.py
@@ -2,15 +2,18 @@ from telegram import Update, ReplyKeyboardRemove
 from telegram.ext import ContextTypes
 import logging
 from logging import Logger
+from states.States import States
 
 class Name:
 
-    async def name(update: Update, context: ContextTypes) -> None:
+    async def name(update: Update, context: ContextTypes) -> int:
+        state = States.getState('idade')
         logger: Logger = logging.getLogger(__name__)
-        user = update.message.from_user
-        logger.info("User name: %s", user.first_name)
+        logger.info("User name: %s", update.message.text)
+        context.user_data["nome"] = update.message.text
         await update.message.reply_text(
-            f"Prazer em te conhecer {user.first_name}! Agora preciso saber se você é homem ou mulher, vamos lá?",
+            f"Prazer em te conhecer {update.message.text}! Agora preciso saber sua idade, vamos lá?",
             reply_markup=ReplyKeyboardRemove()
         )
+        return state
     
