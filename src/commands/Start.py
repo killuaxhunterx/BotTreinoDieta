973c9a7627a185e9e4026734c3bf9f977f8af756
killuaxhunterx <arthurnegaodebsb@gmail.com>
2025-11-14 23:34:15 -0300

    Correção na mensagem do bot

--- src/commands/Start.py
+++ src/commands/Start.py
@@ -1,15 +1,16 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from states.States import States
 
 class Start:
 
     async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        NAME = range(1)
+        state = States.getState("NAME")
 
         await update.message.reply_text(
             "Olá, estou aqui para ajudar você a alcançar seus objetivos relacionado ao seu corpo, " \
             "montando seu treino e sua dieta! Vamos começar? Se sim, primeiramente digite o seu nome. " \
-            "Caso contrário, digite /cancelar para finalizar a conversa."
+            "Caso contrário, digite /cancel para finalizar a conversa."
         )
 
-        return NAME
+        return state
