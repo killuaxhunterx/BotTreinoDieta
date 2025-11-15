63db380e489c1d325fb88e050e7ea892d8af27cc
killuaxhunterx <arthurnegaodebsb@gmail.com>
2025-11-15 17:00:04 -0300

    Alteração na mensagem inical

--- src/commands/Start.py
+++ src/commands/Start.py
@@ -8,7 +8,7 @@ class Start:
         state = States.getState("NAME")
 
         await update.message.reply_text(
-            "Olá, estou aqui para ajudar você a alcançar seus objetivos relacionado ao seu corpo, " \
+            "Olá, meu nome é Dr. Natalia, sou nutricionista e personal trainer e estou aqui para ajudar você a alcançar seus objetivos relacionado ao seu corpo, " \
             "montando seu treino e sua dieta! Vamos começar? Se sim, primeiramente digite o seu nome. " \
             "Caso contrário, digite /cancel para finalizar a conversa."
         )
