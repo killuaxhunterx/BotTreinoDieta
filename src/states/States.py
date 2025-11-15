4ae83b566ddda7bdd93a9d0d465ed1e131f56ba7
killuaxhunterx <arthurnegaodebsb@gmail.com>
2025-11-15 17:03:18 -0300

    Remoção do estado de gerar pdf

--- src/states/States.py
+++ src/states/States.py
@@ -2,23 +2,25 @@
 class States:
 
     _STATE_MAP = {
-            "NAME": 0,
-            "IDADE": 1,
-            "SEXO": 2,
-            "PESO_ATUAL": 3,
-            "DIAS_TREINO": 4,
-            "INTENSIDADE_TREINO": 5,
-            "OBJETIVO": 6,
-            "SAUDE_CONDICAO": 7,
-            "EXPERIENCIA_TREINO": 8,
-            "ONDE_VAI_TREINAR": 9,
-            "HORARIO": 10,
-            "TIPO_ALIMENTO": 11,
-            "ALIMENTOS_RESTRICAO": 12,
-            "QUANTAS_REFEICOES": 13,
-            "HORARIO_FIXO_COMER": 14,
-            "PREPARO_COMIDA": 15,
-            "FACIL_ACESSO_ALIMENTOS": 16
+            "NAME": 1,
+            "IDADE": 2,
+            "SEXO": 3,
+            "PESO_ATUAL": 4,
+            "ALTURA": 5,
+            "DIAS_TREINO": 6,
+            "INTENSIDADE_TREINO": 7,
+            "OBJETIVO": 8,
+            "SAUDE_CONDICAO": 9,
+            "EXPERIENCIA_TREINO": 10,
+            "ONDE_VAI_TREINAR": 11,
+            "HORARIO": 12,
+            "TIPO_ALIMENTO": 13,
+            "ALIMENTOS_RESTRICAO": 14,
+            "QUANTAS_REFEICOES": 15,
+            "HORARIO_FIXO_COMER": 16,
+            "PREPARO_COMIDA": 17,
+            "FACIL_ACESSO_ALIMENTOS": 18,
+            "ENVIAR_PDF": 19
         }
     
     
